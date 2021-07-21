a0a22ed9c771a8f7705604f78cee83c6e7e77cae
MisterManley <george@themanleystudio.com>
2021-07-21 16:37:05 -0500

    fixed some broken CSS

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let party = null;
+let hunger = null;
+let fatigue = null;
+let happiness = null;
+let age = 1;
+
+
+
+
+class monster {
+    constructor(hydration, hunger, fatigue, happiness, age){
+            this.hydration = 75;
+            this.hunger = 65;
+            this.fatigue = 45;
+            this.happiness = 80;
+            this.age = age;
+    }
+
+
+
+
+
+}
